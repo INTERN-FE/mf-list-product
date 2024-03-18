c9f85e37099cfce15b2fa6454f57cff9a58e78f9
Nama <annisanailiya@gmail.com>
2024-03-18 15:12:58 +0700

    list-product

--- src/components/list-product.jsx
+++ src/components/list-product.jsx
@@ -15,17 +15,24 @@ const ListProduct = () => {
 	}, []);
 
 	return (
-		<div>
-			<p>List Product</p>
-			<ul>
+		<div className="container mx-auto">
+			<p className="text-2xl font-semibold mb-4">List Products</p>
+			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
 				{products.map((product) => (
-					<li key={product.id}>
-						{product.title} - {product.price}
-						<img src={product.image} alt="" width={100} />
-					</li>
+					<div key={product.id} className="bg-white rounded-lg shadow-md p-4">
+						<div className="flex justify-center items-center">
+							<img src={product.image} alt={product.title} width={50} height={50} className="mb-4" />
+						</div>
+						<div className="flex flex-col justify-between h-full">
+							<div className="">
+								<p className="text-sm font-semibold mb-2">{product.title}</p>
+								<p className="text-red-500">${product.price}</p>
+							</div>
+						</div>
+					</div>
 				))}
-			</ul>
-		</div>
+			</div>
+		</div>		
 	);
 };
 
